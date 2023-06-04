f7defe0ff034e243724b56155ba08bdfa7908337
Gursevak56 <gursevaksinghgill21@gmail.com>
2023-06-04 13:24:42 +0530

    comment

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
+app.get('/',(req,res)=>[
+   res.send('started')
+])
 const port = process.env.PORT;
 app.listen(port,()=>{
     console.log('server runs on 3000 with very beautiful way');
