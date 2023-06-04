d40234e17a0b6d52429ccad448e804640c9befcb
Gursevak56 <gursevaksinghgill21@gmail.com>
2023-06-04 15:00:08 +0530

    install required modules

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const googlestretage = require('passport-google-oauth20');
 const app = express();
-app.get('/',(req,res)=>[
-   res.send('started with pm2 and new way')
-])
-const port = process.env.PORT;
-app.listen(port,()=>{
+
+const port = process.env.PORT
+const host = process.env.HOST
+app.listen(port,host,()=>{
     console.log('server runs on 3000 with very beautiful way');
 })
