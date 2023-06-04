30e975ecebf77a63cb9947d4a7f3dacb481ad68a
Gursevak56 <gursevaksinghgill21@gmail.com>
2023-06-04 14:06:33 +0530

    pm2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
 app.get('/',(req,res)=>[
-   res.send('started')
+   res.send('started with pm2')
 ])
 const port = process.env.PORT;
 app.listen(port,()=>{
